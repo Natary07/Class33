d985acca3c79d97f778e6ef4b7fcaefe728afd50
Natary07 <aryanpat2007@gmail.com>
2021-01-31 11:45:59 -0600

    bugs

--- Bird.js
+++ Bird.js
@@ -4,6 +4,7 @@ class Bird extends BaseClass {
     this.image = loadImage("sprites/bird.png");
     this.smokeImage = loadImage("sprites/smoke.png");
     this.trajectory =[];
+    this.Visibility=255;
   }
 
   display() {
@@ -11,7 +12,7 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
-
+    
     if(this.body.velocity.x > 10 && this.body.position.x > 200){
       var position = [this.body.position.x, this.body.position.y];
       this.trajectory.push(position);
@@ -19,7 +20,12 @@ class Bird extends BaseClass {
    
 
     for(var i=0; i<this.trajectory.length; i++){
+      push();
+      this.Visibility=this.Visibility - 0.5
+      tint(255,this.Visibility);
       image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+      pop();
     }
+    
   }
 }
